c97b8656cc6ec5970807aea8cb2d38c6334f393a
Onur T <onurtumer@gmail.com>
2018-11-10 20:42:20 +0300

    RegisterUser endpoint, database schema update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 require("dotenv").config()
+const cookieParser = require("cookie-parser")
 const createServer = require("./createServer")
 
 const server = createServer()
+
+server.express.use(cookieParser())
 
 // pass object to start function
 // { cors: { credentials: true, origin: process.env.FRONTEND_URL }}
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
 const Mutations = {
   async createEntry(parent, args, ctx, info) {
     // entry returns a promise
@@ -8,6 +11,30 @@ const Mutations = {
       info
     )
     return entry
+  },
+
+  async registerUser(parent, args, ctx, info) {
+    const email = args.email.toLowerCase()
+    // one-way hash
+    const password = await bcrypt.hash(args.password, 10)
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          name: args.name,
+          email,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    )
+    // sign-in user by setting jwt as a cookie
+    const token = jwt.sign({ userId: user.id }, process.env.PRISMA_SECRET)
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    })
+    return user
   }
 }
 
