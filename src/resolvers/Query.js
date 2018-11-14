e3f0600cb304bab6dfb4942411fd7d6313ca24b2
Onur T <onurtumer@gmail.com>
2018-11-14 22:25:08 +0300

    me endpoint changed to currentUser

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,11 +3,10 @@ const { forwardTo } = require("prisma-binding")
 const Query = {
   // If your query to yoga same as the query to prsima you can forward query w/o writing query for yoga
   entries: forwardTo("db"),
-  me(parent, args, ctx, info) {
-    // returns a promise
+  async currentUser(parent, args, ctx, info) {
     // check if userId on req exists otherwise return null (!No errors otherwise query errors out)
     if (!ctx.request.userId) return null
-    return ctx.db.query.user(
+    return await ctx.db.query.user(
       {
         where: { id: ctx.request.userId }
       },
